d369c4031d20dff4eed73a79afe44a93024af3a0
camisayu <camilasayuri111@gmail.com>
2023-04-24 00:10:17 -0300

    atualizações

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#js").addEventListener("change", calcularOrcamento)
+document.querySelector("#layout_sim").addEventListener("change", calcularOrcamento)
+document.querySelector("#layout_nao").addEventListener("change", calcularOrcamento)
+document.querySelector("#prazo").addEventListener("change", calcularOrcamento)
